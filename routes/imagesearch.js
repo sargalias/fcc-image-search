beefd5b7b4c5408c1640d99d27bee7e13faf77eb
Spyros Argalias <sargalias@gmail.com>
2018-04-10 19:31:28 +0100

    refactor make imageSearch route

--- routes/imagesearch.js
+++ routes/imagesearch.js
@@ -1,34 +1,10 @@
 const router = require('express').Router();
-const googleConfig = require('../config/google');
-const request = require('request');
-const parseGoogleResponse = require('../helpers/parseGoogleResponse');
-const saveQuery = require('../helpers/saveQuery');
 const showLatestQueries = require('../helpers/showLatestQueries');
+const googleQuery = require('../helpers/googleQuery');
 
 
 // Routes
-router.get('/api/imagesearch', (req, res) => {
-    let url = `${googleConfig.baseUrl}?${googleConfig.keyParam}&${googleConfig.cxParam}&${googleConfig.safe}&${googleConfig.fields}`;
-    if (req.query.q) {
-        url += `&q=${req.query.q}`;
-        if (req.query.offset) {
-            url += `&start=${req.query.offset}`;
-        }
-        request.get(url,
-            function(err, req, body) {
-                if (err) {
-                    return res.json(err);
-                }
-                let formattedBody = parseGoogleResponse(body);
-                res.send(formattedBody);
-                saveQuery(this.originalQuery, this.originalOffset);
-            }.bind({originalQuery: req.query.q, originalOffset: req.query.offset})
-        );
-    } else {
-        res.redirect('/error');
-    }
-});
-
+router.get('/api/imagesearch', googleQuery);
 
 router.get('/api/imagesearch/latest', showLatestQueries);
 
