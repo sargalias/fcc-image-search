d37ae22cecdcf5ef5740ade761a04e19fffaa6a4
Spyros Argalias <sargalias@gmail.com>
2018-04-10 13:58:26 +0100

    specify fields in google query for better performance

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const GOOGLE_SE_ID = googleConfig.SEARCH_ENGINE_ID;
 const baseUrl = 'https://www.googleapis.com/customsearch/v1';
 const keyParam = `key=${GOOGLE_API_KEY}`;
 const cxParam = `cx=${GOOGLE_SE_ID}`;
+const safe = 'safe=medium';
+const fields = 'fields=items(title, snippet, link, pagemap)';
+    // Snippet to title
+    // url to link
+    // Thumbnail (small version) - pagemap/cse_thumbnail/src
+    // Full version - cse_image/src
+    // Description is snippet
 
 const app = express();
 
@@ -20,7 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/test', (req, res) => {
     const q = `q=lolcats`;
-    const finalUrl = `${baseUrl}?${keyParam}&${cxParam}&${q}`;
+    const finalUrl = `${baseUrl}?${keyParam}&${cxParam}&${safe}&${q}&${fields}`;
+    // const finalUrl = `${baseUrl}?${keyParam}&${cxParam}&${q}`;
     console.log(finalUrl);
     request.get(finalUrl, (err, req, body) => {
         let reqRes = JSON.parse(body);
