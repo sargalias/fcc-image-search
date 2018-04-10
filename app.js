7148528719baaccca55779a59c7505dcde291fcb
Spyros Argalias <sargalias@gmail.com>
2018-04-10 13:20:59 +0100

    add google API base functionality

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 "use strict";
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const googleConfig = require('./config/google');
 
+
+// Google config and setup
+const GOOGLE_API_KEY = googleConfig.API_KEY;
+const GOOGLE_SE_ID = googleConfig.SEARCH_ENGINE_ID;
+const baseUrl = 'https://www.googleapis.com/customsearch/v1';
+const keyParam = `key=${GOOGLE_API_KEY}`;
+const cxParam = `cx=${GOOGLE_SE_ID}`;
 
 const app = express();
 
@@ -10,8 +19,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.get('/test', (req, res) => {
-    http.get('https://google.com', (res) => {
-        res.send(res);
+    const q = `q=lolcats`;
+    const finalUrl = `${baseUrl}?${keyParam}&${cxParam}&${q}`;
+    console.log(finalUrl);
+    request.get(finalUrl, (err, req, body) => {
+        let reqRes = JSON.parse(body);
+        res.send(reqRes);
     });
 });
 
