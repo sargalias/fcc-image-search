6bccc35922ba9b8f73f2d5b078e27bc5fbf36ee4
Spyros Argalias <sargalias@gmail.com>
2018-04-10 18:54:17 +0100

    add error page for wrong query formats

--- routes/imagesearch.js
+++ routes/imagesearch.js
@@ -11,20 +11,22 @@ router.get('/api/imagesearch', (req, res) => {
     let url = `${googleConfig.baseUrl}?${googleConfig.keyParam}&${googleConfig.cxParam}&${googleConfig.safe}&${googleConfig.fields}`;
     if (req.query.q) {
         url += `&q=${req.query.q}`;
+        if (req.query.offset) {
+            url += `&start=${req.query.offset}`;
+        }
+        request.get(url,
+            function(err, req, body) {
+                if (err) {
+                    return res.json(err);
+                }
+                let formattedBody = parseGoogleResponse(body);
+                res.send(formattedBody);
+                saveQuery(this.originalQuery, this.originalOffset);
+            }.bind({originalQuery: req.query.q, originalOffset: req.query.offset})
+        );
+    } else {
+        res.redirect('/error');
     }
-    if (req.query.offset) {
-        url += `&start=${req.query.offset}`;
-    }
-    request.get(url,
-        function(err, req, body) {
-            if (err) {
-                return res.json(err);
-            }
-            let formattedBody = parseGoogleResponse(body);
-            res.send(formattedBody);
-            saveQuery(this.originalQuery, this.originalOffset);
-        }.bind({originalQuery: req.query.q, originalOffset: req.query.offset})
-    );
 });
 
 
