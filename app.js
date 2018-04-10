758560c91df1a034b22f922c47f56207c59a4ef1
Spyros Argalias <sargalias@gmail.com>
2018-04-10 16:26:56 +0100

    add database connection

--- app.js
+++ app.js
@@ -2,6 +2,15 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes/imagesearch');
+const mongoose = require('mongoose');
+const {DATABASE_URI} = require('./config/database');
+
+
+// Mongoose connection
+mongoose.connect(DATABASE_URI);
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 
 // Express setup
